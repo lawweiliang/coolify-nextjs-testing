442d1bc4a8fcbc11aa9670080a8353ac002bfa54
liang <yeslaw90@hotmail.com>
2024-06-05 14:51:55 +0800

    update:app/pages - Hello World 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Liang Testing NextJs Hello World</h1>
+    <h1>Liang Testing NextJs Hello World 2</h1>
   );
 }
